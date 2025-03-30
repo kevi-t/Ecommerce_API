d7fe22604a0d0b8810d4f9f0c3f4ca0436d2d371
Wafula <kevin.w.wepondi@gmail.com>
2025-03-30 22:27:10 +0300

    chore: update and refactor files

--- ecommerce_rest_api/settings.py
+++ ecommerce_rest_api/settings.py
@@ -2,31 +2,25 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-
+SECRET_KEY = 'django-insecure-ju(nyql!isq_vo7@_1#jh_z$os6(i%lz)01-vbeq@*&9vt0%x2'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
+DEBUG = True
 # Configure Django sites framework
 SITE_ID = 1
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
-
-# Authentication settings
 AUTH_USER_MODEL = 'customer_service.Customer'
 REST_AUTH_TOKEN_MODEL = 'account_service.Token'
 
-# Africastalking SMS Gateway settings
-AFRICASTALKING_USERNAME = os.environ.get('AFRICASTALKING_USERNAME')
-AFRICASTALKING_API_KEY = os.environ.get('AFRICASTALKING_API_KEY')
+#Afrca Talking sms gateway
+AFRICASTALKING_USERNAME = 'sandbox'
+AFRICASTALKING_API_KEY = 'atsk_878fed6f26cb4e196ab242bd5a6df11dc8afee70f2b4dfdd3a925c282ad9f76c6c99a1fc'
 AFRICASTALKING_API_URL = 'https://api.sandbox.africastalking.com/version1/messaging'
 
 # OIDC configuration
-OIDC_RP_CLIENT_ID = os.environ.get('OIDC_RP_CLIENT_ID')
-OIDC_RP_CLIENT_SECRET = os.environ.get('OIDC_RP_CLIENT_SECRET')
+OIDC_RP_CLIENT_ID = '638828902981-jst85g7pdgng9rpi2fqg2su0pg1ph526.apps.googleusercontent.com'
+OIDC_RP_CLIENT_SECRET = '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'
 OIDC_OP_AUTHORIZATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/v2/auth'
 OIDC_OP_TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/token'
 OIDC_OP_USERINFO_ENDPOINT = 'https://openidconnect.googleapis.com/v1/userinfo'
@@ -50,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
-    # OpenID Connect provider config
+     # OpenID Connect provider config
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -69,7 +63,10 @@ INSTALLED_APPS = [
 # Token Authentication settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+       # 'rest_framework.authentication.SessionAuthentication',
+       # 'rest_framework.authentication.BasicAuthentication',
+       # 'rest_framework.authentication.TokenAuthentication',
+       'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
@@ -135,11 +132,11 @@ WSGI_APPLICATION = 'ecommerce_rest_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'ecommerce_db'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'java'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'ecommerce_db',
+        'USER': 'postgres',
+        'PASSWORD': 'java',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
