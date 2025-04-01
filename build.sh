2f822a7970c217cfdad95178aaedd8c81cccb159
Wafula <kevin.w.wepondi@gmail.com>
2025-04-01 13:51:57 +0300

    fix: fix deployment issues on vercel

--- build.sh
+++ build.sh
@@ -13,5 +13,4 @@ echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+echo "Build process completed successfully."
