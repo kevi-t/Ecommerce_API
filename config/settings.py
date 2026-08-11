85052b246d1f68f9bfd587b1ab4f0d1ca05c6b25
kevin(w)wepondi <104903442+kevi-t@users.noreply.github.com>
2026-08-12 00:19:07 +0300

    chore: update and migrate from supabase

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-_db_url = env('POSTGRES_URL_NON_POOLING', default=env('DATABASE_URL', default=''))
+_db_url = env('DATABASE_URL', default=env('DATABASE_URL', default=''))
 DATABASES = {
     'default': dj_database_url.config(default=_db_url, conn_max_age=0, ssl_require=True)
 }
